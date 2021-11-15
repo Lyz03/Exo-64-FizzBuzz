49a0514a815ff4c4f816502125d7a3b7cc1bd954
Lyz03 <lizoe@netcourrier.com>
2021-11-15 18:23:49 +0100

    exo 64

---
+++ app.js
@@ -0,0 +1,20 @@
+
+for (let i = 0; i <= 100; i++) {
+
+    if ((i % 3) === 0 && (i % 5) !== 0) {
+        console.log("fizz");
+    }
+
+    if ((i % 5) === 0 && (i % 3) !== 0) {
+        console.log("buzz");
+    }
+
+    if ((i % 3) === 0 && (i % 5) === 0) {
+        console.log("fizzbuzz");
+    }
+
+    if ((i % 3) !== 0 && (i % 5) !== 0) {
+        console.log("nombre");
+    }
+}
+
